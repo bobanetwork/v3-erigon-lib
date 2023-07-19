c4507a07fa04709234e899f8f5bb66fbdda0db9c
cby3149 <cby3149@gmail.com>
2023-07-19 14:51:31 -0700

    Fix lint and change code order

--- chain/chain_config.go
+++ chain/chain_config.go
@@ -68,7 +68,7 @@ type Config struct {
 	PragueTime   *big.Int `json:"pragueTime,omitempty"`
 
 	// Optimism Forks
-	BedrockBlock *big.Int `json:"bedrockBlock,omitempty" toml:,omitEmpty"` // bedrockSwitch block (nil = no fork, 0 = already actived)
+	BedrockBlock *big.Int `json:"bedrockBlock,omitempty"` // bedrockSwitch block (nil = no fork, 0 = already actived)
 	// RegolithTime is *uint64 in op-geth
 	RegolithTime *big.Int `json:"regolithTime,omitempty"` // Regolith switch time (nil = no fork, 0 = already on optimism regolith)
 
@@ -217,17 +217,23 @@ func (c *Config) IsRegolith(time uint64) bool {
 	return isForked(c.RegolithTime, time)
 }
 
-func (c *Config) IsOptimismBedrock(num uint64) bool {
-	return c.Optimism != nil && c.IsBedrock(num)
+// IsOptimism returns whether the node is an optimism node or not.
+func (c *Config) IsOptimism() bool {
+	return c.Optimism != nil
 }
 
-func (c *Config) IsOptimismPreBedrock(num uint64) bool {
-	return c.Optimism != nil && !c.IsBedrock(num)
+func (c *Config) IsOptimismBedrock(num uint64) bool {
+	return c.IsOptimism() && c.IsBedrock(num)
 }
 
 func (c *Config) IsOptimismRegolith(time uint64) bool {
 	// Optimism op-geth has additional complexity which is not yet ported here.
 	return /* c.IsOptimism() && */ c.IsRegolith(time)
+}
+
+// IsOptimismPreBedrock returns true iff this is an optimism node & bedrock is not yet active
+func (c *Config) IsOptimismPreBedrock(num uint64) bool {
+	return c.IsOptimism() && !c.IsBedrock(num)
 }
 
 func (c *Config) IsEip1559FeeCollector(num uint64) bool {
